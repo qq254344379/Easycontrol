3cfe733cfdb783d4b427abc1786bc284b181e280
love168 <65702480+qq254344379@users.noreply.github.com>
2026-04-23 14:51:17 +0800

    Add stub ActiveHelper.java (bypass activation check)

---
+++ easycontrol/app/src/main/java/top/saymzx/easycontrol/app/helper/ActiveHelper.java
@@ -0,0 +1,11 @@
+package top.saymzx.easycontrol.app.helper;
+
+public class ActiveHelper {
+    public static boolean active(String key) {
+        return true; // Always return true - activation bypassed
+    }
+
+    public static boolean deactivate(String key) {
+        return true;
+    }
+}
